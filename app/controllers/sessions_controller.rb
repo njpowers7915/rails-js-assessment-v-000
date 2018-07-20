e2ac0351d3280f83d55c9441239601dd5e841043
Nicholas Powers <njpowers7915@gmail.com>
2018-07-20 18:27:49 +0000

    Methods for Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      redirect_to @user
+    else
+      #Error Message
+      flash[:danger] = 'Invalid name/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/'
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
+  root "welcome#home"
+
+  get '/login', to: "users#new"
+  post '/login', to: "users#create"
+  get '/singup', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
   resources :users
   resources :playlists
   resources :songs
   #resources :users
   #resources :rides
   #resources :attractions
-
-  #root "welcome#home"
-
-  #get '/signup', to: "users#new"
-  #post '/signup', to: "users#create"
-  #get '/signin', to: "sessions#new"
-  #post '/signin', to: "sessions#create"
-  #get '/logout', to: "sessions#destroy"
 end
