aa8cb75054fcaee268e7a5ccd6e9c57461a65139
Nicholas Powers <njpowers7915@gmail.com>
2018-07-20 18:36:33 +0000

    Methods built out for Login/Logout/Signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.find_by(name: params[:session][:name])
     if @user && @user.authenticate(params[:session][:password])
-      log_in(@user)
-      redirect_to @user
+      #log_in(@user)
+      session[:user_id] = @user.id
+      redirect_to @user#, notice: "Welcome back to APP NAME"
     else
       #Error Message
-      flash[:danger] = 'Invalid name/password combination'
-      render 'new'
+      #flash[:danger] = 'Invalid name/password combination'
+      redirect_to signin_path
     end
   end
 
   def destroy
-    log_out
+    session[:user_id] = nil
     redirect_to '/'
   end
-end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,55 @@
 class UsersController < ApplicationController
 
-  def new
+  ef new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #if user_params[:admin] == "0"
+        session[:user_id] = @user.id
+        #format.html { redirect_to @user, notice: "Welcome to APP NAME!" }
+        redirect_to @user
+      #elsif user_params[:admin] == "1"
+      #  admin_signup
+      #end
+    else
+      render 'new'
+      #format.html { render :new }
+    end
   end
 
+  def show
+    if User.find_by_id(session[:user_id])
+      @user = User.find_by_id(session[:user_id])
+      #@attraction = Attraction.find_by_id(session[:attraction_id])
+
+    else
+      redirect_to '/'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :admin)
+  end
 
 
 end
