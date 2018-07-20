48caf9a97e683c258f21e407359a06b9d8d7ead8
Nicholas Powers <njpowers7915@gmail.com>
2018-07-20 20:08:36 +0000

    Song Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
-  get '/singin', to: "sessions#new"
-  post '/signin', to: "sessions#create"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
-  
+
 end
