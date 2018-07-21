d82d8feba9bec6684908d7dbed26b1f6c31a2ed4
Nicholas Powers <njpowers7915@gmail.com>
2018-07-21 13:34:40 +0000

    Updating New Playlist form

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,27 @@
 class PlaylistsController < ApplicationController
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    if @song.save
+      #if user_params[:admin] == "0"
+        session[:song_id] = @song.id
+        #format.html { redirect_to @user, notice: "Welcome to APP NAME!" }
+        redirect_to @song
+      #elsif user_params[:admin] == "1"
+      #  admin_signup
+      #end
+    else
+      render 'new'
+      #format.html { render :new }
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist, :genre)
+  end
 end
