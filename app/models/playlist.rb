4b852256a8c8fb95f9f4f50c309930d1b4e777eb
Nicholas Powers <njpowers7915@gmail.com>
2018-07-21 14:20:58 +0000

    Updating User Show and New Playlist views

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,4 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :songs, throug: :song_playlists
+  has_many :songs, through: :song_playlists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :songs, through: :playlists 
+  has_many :playlists
 end
