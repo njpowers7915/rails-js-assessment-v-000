d19e2d3b48022a261a180eaf44da1055daea1b9f
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 12:16:25 +0000

    Making nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users do
+    resources :playlists, only: [:new, :create, :show]
+
+
   resources :playlists
   resources :songs
 
