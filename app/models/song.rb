ef279a46da1f15a75f4d133b621906b8c4a3a3a3
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 13:31:17 +0000

    Creating song class methods

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,11 @@ class Song < ApplicationRecord
   has_many :users, through: :playlists
   has_many :song_playlists
   has_many :playlists, through: :song_playlists
+
+  def add_song
+  end
+
+  def play_song
+  end
+
 end
